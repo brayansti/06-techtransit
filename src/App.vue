d8b1c602553e3166f911025b5f08b59c8f274d9a
Brayan Camargo <brayan440@hotmail.com>
2020-01-21 21:08:42 -0500

    Before api config

--- src/App.vue
+++ src/App.vue
@@ -16,25 +16,19 @@
               </div>
               <div class="itemBox__divider itemBox__divider--bottom">
                 <div>
-                  <h2>Lorem ipsum dolor</h2>
-                  <p>
+                  <h2 class="itemBox__title">Lorem ipsum dolor</h2>
+                  <p class="itemBox__description">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   </p>
                 </div>
-                <div class="">
-                  <star-rating v-bind="{rating: number, starStyle:starStyle}"></star-rating>
+                <div class="d-flex justify-content-between">
+                  <div>
+                    <h2 class="itemBox__title itemBox__price">$14.15</h2>
+                  </div>
+                  <div>
+                    <input v-model="test" type="number"> {{number}}
+                  </div>
                 </div>
-                <div class="">
-                  <star-rating v-bind="{rating: 2.1, starStyle:starStyle}"></star-rating>
-                </div>
-                <div class="">
-                  <star-rating v-bind="{rating: 1.1, starStyle:starStyle}"></star-rating>
-                </div>
-                <div class="">
-                  <star-rating :config="config"></star-rating>
-                </div>
-                <input v-model="test" type="number">
-                {{number}}
               </div>
             </div>
           </article>
@@ -49,13 +43,11 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import StarRating from 'vue-dynamic-star-rating'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-    StarRating
+    HelloWorld
   },
   data: function() {
     return {
@@ -87,51 +79,4 @@ export default {
 
 <style lang="scss">
 @import "./scss/main.scss";
-// #app {
-//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
-
-.itemBox{
-  &__container{
-    border: 1px solid gray;
-  }
-  &__divider{
-    padding: 0 20px;
-    &--top{
-      height: 50px;
-      display: flex;
-      position: relative;
-      align-items: center;
-      justify-content: flex-end;
-      background-color: lightgray;
-    }
-    &--bottom{
-      padding-top: 20px;
-      padding-bottom: 20px;
-    }
-  }
-  &__image{
-    top: 10px;
-    left: 20px;
-    position: absolute;
-    width: 60px;
-    img{
-      width: 100%;
-      display: block;
-    }
-  }
-  &__credits{
-    padding: 10px;
-    background: $white;
-    border-radius: 10px;
-    letter-spacing: .6px;
-    @include fontSize(12);
-  }
-
-}
 </style>
