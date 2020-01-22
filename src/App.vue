441aa2f45f03dcc5e0a67e84d16006360f7e9a1e
Brayan Camargo <brayan440@hotmail.com>
2020-01-21 22:47:28 -0500

    Api conected

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,111 @@
 <template>
   <div id="app">
-
+    <!--  -->
     <div class="container">
       <div class="row">
-        <div class="col-md-4">
-          <article class="itemBox">
+        <div class="col-md-12">
+          <input v-model="newsSearch" @keyup="changeInput" type="text">
+        </div>
+      </div>
+      <div class="row">
+        <div v-for="(item, $index) in list" :key="$index" class="col-md-4">
+          <a target="_blank" :href="item.url" class="itemBox">
             <div class="itemBox__container">
               <div class="itemBox__divider itemBox__divider--top">
                 <figure class="itemBox__image">
-                  <img src="https://mytablemesa.com/Content/uploads/684a5f05-af87-438e-b32a-82bf7e92e729.png" alt="Image">
+                  <!-- <img src="https://mytablemesa.com/Content/uploads/684a5f05-af87-438e-b32a-82bf7e92e729.png" alt="Image"> -->
+                  <img :src="'https://i.picsum.photos/id/'+item.points+'/400/400.jpg'" :alt="item.title">
                 </figure>
                 <div class="itemBox__credits">
-                  1 credits
+                  {{item.points}} credits
                 </div>
               </div>
               <div class="itemBox__divider itemBox__divider--bottom">
                 <div>
-                  <h2 class="itemBox__title">Lorem ipsum dolor</h2>
+                  <h2 class="itemBox__title">
+                    {{item.author}}
+                  </h2>
                   <p class="itemBox__description">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    {{item.title}}
                   </p>
                 </div>
                 <div class="d-flex justify-content-between">
                   <div>
-                    <h2 class="itemBox__title itemBox__price">$14.15</h2>
-                  </div>
-                  <div>
-                    <input v-model="test" type="number"> {{number}}
+                    <h2 class="itemBox__title itemBox__price">
+                      <!-- $14.15 -->
+                      ${{item.objectID}}
+                    </h2>
                   </div>
                 </div>
               </div>
             </div>
-          </article>
+          </a>
         </div>
       </div>
     </div>
+    <div infinite-wrapper>
+      <infinite-loading force-use-infinite-wrapper :identifier="infiniteId" @infinite="infiniteHandler"></infinite-loading>
+    </div>
 
-
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--  -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import InfiniteLoading from 'vue-infinite-loading';
+
+const api = '//hn.algolia.com/api/v1/search_by_date?tags=story';
+// const api = '//test.mytablemesa.com/api/courses?orderBy=popularity+desc&expand=provider&limit=24&profession=&subjectAreaCode=&state=&provider=&name=';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    InfiniteLoading
   },
-  data: function() {
+  data() {
     return {
-      test: 0,
-      starStyle: {
-        fullStarColor: "#ed8a19",
-        emptyStarColor: "#737373",
-        starWidth: 20,
-        starHeight: 20
-      },
-        config: {
-            rating: 2.5,
-            style: {
-                fullStarColor: '#ed8a19',
-                emptyStarColor: '#737373',
-                starWidth: 30,
-                starHeight: 30
-            }
-        }
-    }
+      page: 1,
+      list: [],
+      newsSearch: '',
+      infiniteId: +new Date(),
+    };
   },
-  computed:{
-    number: function(){
-      return parseInt(this.test)
+  methods: {
+    infiniteHandler($state) {
+      axios.get(api, {
+        params: {
+          tags: 'story',
+          page: this.page,
+          query: this.newsSearch
+        },
+      }).then(({ data }) => {
+        if (data.hits.length) {
+          this.page += 1;
+          this.list.push(...data.hits);
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+      });
     },
-  }
+    changeInput() {
+      let timeout = null;
+      clearTimeout(timeout);
+      // Delay search
+      timeout = setTimeout(() => {
+        this.page = 1;
+        this.list = [];
+        this.infiniteId += 1;
+      }, 800);
+    },
+  },
+
+  // computed:{
+  //   number: function(){
+  //     return parseInt(this.test)
+  //   },
+  // }
 }
 </script>
 
